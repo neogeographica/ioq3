7865a4576658cb1d54aefe5440e55f8d4e07311e
Joel Baxter <joel.baxter@neogeographica.com>
2018-07-03 13:21:44 -0700

    make modbuild script friendlier for other Unix/Linux
    Let's try not to be so Mac-centric. Not sure this will really do the trick but it's probably a step in the right direction.

--- modbuild/modbuild.sh
+++ modbuild/modbuild.sh
@@ -1,22 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 cd ..
-make BUILD_STANDALONE=0 BUILD_CLIENT=0 BUILD_SERVER=0 BUILD_GAME_SO=0 BUILD_GAME_QVM=1 BUILD_BASEGAME=1 BUILD_MISSIONPACK=1 BUILD_RENDERER_OPENGL2=0
+CMDOUT=`make BUILD_STANDALONE=0 BUILD_CLIENT=0 BUILD_SERVER=0 BUILD_GAME_SO=0 BUILD_GAME_QVM=1 BUILD_BASEGAME=1 BUILD_MISSIONPACK=1 BUILD_RENDERER_OPENGL2=0 release 2>&1 | tee /dev/tty`
 
-pushd build/release-darwin-x86_64/baseq3
+if [[ $CMDOUT =~ Error\ [0-9]+$ ]]; then
+  exit 1
+fi
+
+if [[ $CMDOUT =~ Building\ in\ ([^:]*): ]]; then
+  BUILD_DIR=${BASH_REMATCH[1]}
+  echo
+  echo "Picking up qvm files under $BUILD_DIR..."
+else
+  echo
+  echo "couldn't find build directory in make output"
+  exit 1
+fi
+
+pushd $BUILD_DIR/baseq3
 zip pak0.zip vm/cgame.qvm vm/qagame.qvm
 popd
 pushd modbuild
-mv -f ../build/release-darwin-x86_64/baseq3/pak0.zip surveyor/pak0.pk3
+mv -f ../$BUILD_DIR/baseq3/pak0.zip surveyor/pak0.pk3
 zip -r surveyor.zip surveyor
 popd
 
-pushd build/release-darwin-x86_64/missionpack
+pushd $BUILD_DIR/missionpack
 zip pak0.zip vm/cgame.qvm vm/qagame.qvm
 popd
 pushd modbuild
-mv -f ../build/release-darwin-x86_64/missionpack/pak0.zip surveyor_ta/pak0.pk3
+mv -f ../$BUILD_DIR/missionpack/pak0.zip surveyor_ta/pak0.pk3
 zip -r surveyor_ta.zip surveyor_ta
 popd
